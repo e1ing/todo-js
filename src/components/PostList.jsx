b3103fdeef6f6f0535b2c2199eb18b0e8b51b2d1
e1ing <elvira.kisling@gmail.com>
2022-12-29 14:55:04 +0700

    effects on post (for delet and add post)

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import { PostItem } from './PostItem';
+import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { PostItem } from "./PostItem";
 
 export const PostList = ({ posts, title, remove }) => {
-
-   if (!posts.length) {
-      return (
-         <h1 style={{ textAlign: "center" }}>Posts not found...</h1>
-      )
-   }
-   return (
-      <div className="App">
-         <h1 style={{ textAlign: 'center' }}>{title}</h1>
-         <div>  
-            {posts.map((post, index) =>
-               <PostItem 
-               key={post.id}
-               number={index + 1}
-               post={post} 
-               remove = {remove}
-                />
-            )}
-         </div>
+  if (!posts.length) {
+    return <h1 style={{ textAlign: "center" }}>Posts not found...</h1>;
+  }
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      <div>
+        <TransitionGroup>
+          {posts.map((post, index) => (
+            <CSSTransition key={post.id} timeout={500} classNames="post">
+              <PostItem number={index + 1} post={post} remove={remove} />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       </div>
-   )
-}
+    </div>
+  );
+};
