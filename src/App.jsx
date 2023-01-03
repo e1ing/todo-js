feec3b75a6105b531367f567ad2d1355901bf19a
e1ing <elvira.kisling@gmail.com>
2023-01-03 17:16:10 +0700

    Pagination created

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,34 @@ import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/postService";
 import { Loader } from "./components/UI/loader/Loader";
 import { useFetching } from "./hooks/useFetching";
+import { getPageCount } from "./utils/pages";
+import { Pagination } from "./components/UI/pagination/Pagination";
 
 export const App = () => {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit] = useState(10);
+  const [page, setPage] = useState(1);
 
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll();
-    setPosts(response.data);
-  });
+  const [fetchPosts, isPostLoading, postError] = useFetching(
+    async (limit, page) => {
+      const response = await PostService.getAll(limit, page);
+      setPosts(response.data);
+      const totalCount = response.headers["x-total-count"];
+      setTotalPages(getPageCount(totalCount, limit));
+    }
+  );
 
   useEffect(() => {
-    fetchPosts();
+    fetchPosts(limit, page);
   }, []);
 
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts(limit, page);
+  };
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
@@ -65,6 +78,12 @@ export const App = () => {
           remove={removePost}
         />
       )}
+
+      <Pagination 
+      page={page} 
+      changePage={changePage} 
+      totalPages={totalPages} />
+      
     </div>
   );
 };
