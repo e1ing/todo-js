56562143f86e44a7feb1113dc1e6698ba7e7f65c
e1ing <elvira.kisling@gmail.com>
2023-01-11 22:16:55 +0700

    routing fixed

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { About } from "../pages/About";
+import { Posts } from "../pages/Posts";
+import { Error } from "../pages/Error";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/error" element={<Error />} />
+      <Route path="/" element={<Navigate to="/posts" replace />} />
+      <Route path="*" element={<Navigate to="/error" replace />} />
+    </Routes>
+  );
+};
